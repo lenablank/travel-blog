aaf7f66ef783c84cc3d838420b07a168fcb1309b
Lena Blank <mine.blank@gmail.com>
2021-02-12 16:33:11 +0100

    add validations and new page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,22 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = 'Article was successfully created'
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :body, :country, :city)
+  end
 end
