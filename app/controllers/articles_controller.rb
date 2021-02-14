19ec02ca6525b2ebcbed55019dce5848ac4d57cf
Lena Blank <mine.blank@gmail.com>
2021-02-14 16:21:00 +0100

    add edit action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,21 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = 'Article was updated'
+      redirect_to article_path(@article)
+    else
+      flash[:notice] = 'Article was not updated'
+      render :edit
+    end
+  end
+
   def article_params
     params.require(:article).permit(:title, :body, :country, :city)
   end
